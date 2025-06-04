c4d453bbc14d4e99d9dff84537ff66d9ca646829
Benny Beer <bennybe@std.hac.ac.il>
2025-06-04 17:35:18 +0300

    Level.h created

--- include/Gameplay/Level.h
+++ include/Gameplay/Level.h
@@ -1,12 +1,32 @@
 #pragma once
-#include "Gameplay/Enemy.h"
 #include <vector>
 #include "SFML/Graphics.hpp"
+
+#include "Gameplay/Enemy.h"
+#include "Objects/PickableObject.h"
+// string\intאיך מייצגים שלב? 
+//אולי std::vector<std::unique_ptr<Enemy>> m_enemies;
+//std::vector<std::unique_ptr<PickableObject>> m_pickables;
+//האם השלב עושה run, render, update?
+
 class Level {
 public:
+	Level(int levelName);
+	void run();
+	void update();
+	void render(sf::RenderWindow& window);
+
+
 
 private:
+	void loadLevel(const std::string& levelName);
+	
 	std::vector<Enemy> m_enemies;
+	std::vector<PickableObject> m_pickable;
+	
+
 	sf::Sprite m_backgroundSprite;
 	sf::Texture m_backgroundTexture;
+
+	std::string m_levelName
 };
