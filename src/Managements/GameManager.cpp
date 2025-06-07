c189ed498fcba16e4ab887a3cf1431ec61db3ab3
AdamSimon1106 <adam.simonov@gmail.com>
2025-06-07 13:53:52 +0300

    #defined run()

--- src/Managements/GameManager.cpp
+++ src/Managements/GameManager.cpp
@@ -12,6 +12,39 @@ GameManager::GameManager() : m_window(sf::VideoMode(500, 500), "Little Fighter 2
                              m_currState(std::make_unique<InGameState>(m_window, *this))
 {};
 
+void GameManager::run()
+{
+    sf::Clock clock;
+    while (m_window.isOpen())
+    {
+        sf::Event ev;
+        while (m_window.pollEvent(ev))
+        {
+            if (ev.type == sf::Event::Closed)
+            {
+                m_window.close();
+            }
+            else if (ev.type == sf::Event::KeyReleased && ev.key.code == sf::Keyboard::Escape)
+            {
+                m_window.close();
+            }
+
+            sf::Time delateTime = clock.restart();
+
+            m_currState->update(deltaTime);
+            m_window.clear();
+            m_currState -> render();
+            m_window.display();
+            
+            if (!m_nextState)
+            {
+                m_currState = std::move(m_nextState);
+            }
+
+        }
+    }
+}
+
 void GameManager::switchState(std::unique_ptr<IState> nextState)
 {
     m_nextState = std::move(nextState);
