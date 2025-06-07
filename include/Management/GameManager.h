7d82d85f3dc6d38d7a7da2413c667ea7bada4371
AdamSimon1106 <adam.simonov@gmail.com>
2025-06-07 12:50:50 +0300

    GameManager modifications # 1. now holds the window, the cloock and pointers to the current and next state # 2. switchState() now takes a pointer to the next state # 3. run() was added to be the function the is inchage of delegating everything to the state

--- include/Management/GameManager.h
+++ include/Management/GameManager.h
@@ -3,6 +3,7 @@
 #include "Management/ResourceManager.h"
 #include <memory>
 #include <vector>
+#include <SFML/Graphics.hpp>
 #include "GamePlay/Level.h"
 #include "Management/Controller.h"
 
@@ -10,11 +11,15 @@
 
 class GameManager {
 public:
-	void switchState(IState& state);
+	void switchState(std::unique_ptr<IState> nextState);
 	void readLevels(std::string path);
+	void run();
 
 private:
-	std::unique_ptr<IState> m_state;
+	std::unique_ptr<IState> m_currState;
+	std::unique_ptr<IState> m_nextState = nullptr;
+	sf::RenderWindow m_window;
+	sf::Clock m_clock;
 	ResourceManager m_recourceManager;
 	std::vector<Level> m_levels;
 	Controller m_controller;
