33a0359699f5bd6e6f4fcfb44d81a3e5548e35a2
AdamSimon1106 <adam.simonov@gmail.com>
2025-06-08 11:45:10 +0300

    #

--- include/Management/GameManager.h
+++ include/Management/GameManager.h
@@ -3,21 +3,21 @@
 #include "Management/ResourceManager.h"
 #include <memory>
 #include <vector>
+#include <SFML/Graphics.hpp>
 #include "GamePlay/Level.h"
 #include "Management/Controller.h"
 
-
-
 class GameManager {
 public:
-	void switchState(IState& state);
+	GameManager();
+	void switchState(std::unique_ptr<IState> nextState);
 	void readLevels(std::string path);
 	void run();
 
 private:
-	std::unique_ptr<IState> m_state;
-	ResourceManager m_recourceManager;
+	sf::RenderWindow m_window;
 	std::vector<Level> m_levels;
-	Controller m_controller;
+	std::unique_ptr<IState> m_currState;
+	std::unique_ptr<IState> m_nextState = nullptr;
 	
 };
