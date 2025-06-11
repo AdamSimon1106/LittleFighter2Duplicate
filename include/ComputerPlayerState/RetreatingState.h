ff531699d3c268325cefd8b8af54ca9f6bf71c1c
omershi <omershi@edu.hac.ac.il>
2025-06-11 14:14:36 +0300

    ReatritingState pseudo

--- include/ComputerPlayerState/RetreatingState.h
+++ include/ComputerPlayerState/RetreatingState.h
@@ -10,6 +10,7 @@ public:
     void exit(ComputerPlayer& player) override;
 
 private:
+    // time of retreating can also be determined according to hp level, ect.
     float m_retreatDuration = 2.0f; // Duration of retreating
     float m_elapsedTime = 0.0f;
 };
