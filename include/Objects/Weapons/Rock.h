ab823455e17213722079cfe74b80c6cb7f87961c
Benny Beer <bennybe@std.hac.ac.il>
2025-06-15 14:26:07 +0300

    Add new `Rock` class inheriting from `Weapon`
    Introduced the `Rock` class as a specialized weapon type.
    Defined the class in `Rock.h` and implemented it in `Rock.cpp`.
    Added `#pragma once` in `Rock.h` for single inclusion.
    Included `Weapon` header for inheritance.
    Implemented `Rock` constructor to initialize the base `Weapon`
    class with position and name parameters.

---
+++ include/Objects/Weapons/Rock.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Objects/Weapon.h"
+
+class Rock : public Weapon
+{
+public:
+	Rock(const sf::Vector2f pos, const std::string& name);
+private:
+
+};
---
+++ src/Objects/Weapons/Rock.cpp
@@ -0,0 +1,6 @@
+#include "Objects/Weapons/Rock.h"
+
+Rock::Rock(const sf::Vector2f pos, const std::string& name
+	:Weapon(pos, name)
+{
+}
